8850fdadc35982f6a2f2e52561958bfd4d7478c6
Maszz <mawin38408@gmail.com>
2024-02-04 13:42:06 +0700

    add copilot

--- plugins.lua
+++ plugins.lua
@@ -31,6 +31,18 @@ local plugins = {
     },
   },
   {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enable = false,
+      },
+      panel = {
+        enable = false,
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
@@ -86,6 +98,27 @@ local plugins = {
     build = function()
       vim.cmd [[silence! GoInstallDeps]]
     end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
   },
 }
 
